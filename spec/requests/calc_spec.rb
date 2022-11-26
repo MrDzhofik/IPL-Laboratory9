9cdd438d7f17a68cc9f54d91b42b6e041be7fcf7
MrDzhofik <djafarovemil04@mail.ru>
2022-11-26 18:28:55 +0300

    tests by me & Selenium IDE

--- spec/requests/calc_spec.rb
+++ spec/requests/calc_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "Calcs", type: :request do
   describe "GET /input" do
     it "returns http success" do
-      get "/calc/input"
+      get "/"
       expect(response).to have_http_status(:success)
     end
   end
