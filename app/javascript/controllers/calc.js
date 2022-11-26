7da93adff9bc836d3f19dad92e77f9f81535dad6
MrDzhofik <djafarovemil04@mail.ru>
2022-11-26 17:08:59 +0300

    it works!

--- app/javascript/controllers/calc.js
+++ app/javascript/controllers/calc.js
@@ -1,12 +1,10 @@
-function show_result(data) {
-    var result = document.getElementById("result");
-    result.innerHTML = "<hr/>Result is: " + data.value +
-        "<hr/><p>" + Date() + "</p>";
-}
+
 $(document).ready(function () {
-    $("#calc_form").bind("ajax:success",
+    $("#input_form").bind("ajax:success",
         function (xhr, data, status) {
             // data is already an object
             show_result(data)
         })
 })
+
+console.log("5")
